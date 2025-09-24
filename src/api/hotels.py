ba87b22563cbdf8d4de2afcad6da766ec5f90e27
Sergey Chilingaryan <s.chilingaryan94@gmail.com>
2025-09-25 00:38:43 +0400

    last changes

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,5 +1,3 @@
-from http import HTTPStatus
-
 from fastapi.params import Query
 from fastapi import status, Response, APIRouter, Body
 from repositories.hotels import HotelsRepository
@@ -31,9 +29,8 @@ async def update_hotel_params(hotel_id: int,
                               response: Response,
                               hotel_model: Hotels
                               ):
-    filter_by = {"id": hotel_id}
     async with async_session_maker() as session:
-        res = await HotelsRepository(session).edit(data=hotel_model, **filter_by)
+        res = await HotelsRepository(session).edit(data=hotel_model, id=hotel_id)
         await session.commit()
         if len(res) == 0:
             response.status_code = status.HTTP_404_NOT_FOUND
@@ -46,28 +43,18 @@ async def update_hotel_params(hotel_id: int,
 
 @router.patch("/{hotel_id}")
 async def update_hotel_param(hotel_id: int,
-                             response: Response,
                              hotel_data: HotelsPatch):
-    global hotels
-    if not hotel_data.title and not hotel_data.name:
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        return {"status": 'error', 'message': "No params for Patch"}
-    for hotel in hotels:
-        if hotel['id'] == hotel_id:
-            if hotel_data.name:
-                hotel["name"] = hotel_data.name
-            if hotel_data.title:
-                hotel['title'] = hotel_data.title
-            return hotels
-    response.status_code = status.HTTP_400_BAD_REQUEST
-    return {"status": 'error', 'message': f"There is no Hotel with id: {hotel_id}"}
+    async with async_session_maker() as session:
+        await HotelsRepository(session).edit(data=hotel_data, exclude_unset=True, id=hotel_id)
+        await session.commit()
+
+    return {"status": "OK"}
 
 
 @router.delete('/{hotel_id}')
 async def delete_hotel(hotel_id: int, response: Response):
-    filter_by = {"id": hotel_id}
     async with async_session_maker() as session:
-        res = await HotelsRepository(session).delete(model_id=hotel_id, **filter_by)
+        res = await HotelsRepository(session).delete(id=hotel_id)
         await session.commit()
     if len(res) == 0:
         response.status_code = status.HTTP_404_NOT_FOUND
@@ -90,3 +77,5 @@ async def create_hotel(hotel_data: Hotels = Body(
         hotel = await HotelsRepository(session).add(hotel_data)
         await session.commit()
     return {"status": "OK", "inserted_data": hotel}
+
+
