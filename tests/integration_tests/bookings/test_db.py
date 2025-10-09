add4c6c9681163ad4048494d09142a86a4439290
Sergey Chilingaryan <s.chilingaryan94@gmail.com>
2025-10-09 04:01:43 +0400

    booking crud test update

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -19,11 +19,21 @@ async def test_add_booking(db):
 
     find_new_added_booking = await db.bookings.get_one_or_none(id=new_added_booking.id)
     assert find_new_added_booking
-    updated_booking_data = BookingsAddRequest(
-        room_id=booking_data.room_id,
+    updated_booking_data = BookingsRequest(
+        user_id=user_id,
+        room_id=room_id,
         date_from=date(year=2025, month=10, day=11),
-        date_to=date(year=2025, month=10, day=22)
+        date_to=date(year=2025, month=10, day=22),
+        price=100
     )
+
     await db.bookings.edit(updated_booking_data, exclude_unset=True, id=find_new_added_booking.id)
+    updated_booking = await db.bookings.get_one_or_none(id=find_new_added_booking.id)
+    assert updated_booking
+    assert updated_booking.id == new_added_booking.id
+    assert updated_booking.date_to == updated_booking_data.date_to
+
     await db.bookings.delete(id=find_new_added_booking.id)
-    await db.commit()
+    booking = await db.bookings.get_one_or_none(id=find_new_added_booking.id)
+    assert not booking
+
